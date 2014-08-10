94e08216d8dae49614a079db6723ae3c9d4e4b47
Jefferson Campos <foguinho.peruca@gmail.com>
2014-08-10 10:42:05 -0300

    Fields
    Added basic fields to work

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -11,14 +11,26 @@
 	'use strict';
 
 	return {
-		url: undefined,
+		version: '3.0',
+		baseUrl: 'http://spreadsheet.google.com/feeds/',
+		feed: 'list',
 		key: undefined,
+		worksheet: 1,
+		visibility: 'public',
+		projection: 'full',
 
 		extend: function(args) {
+			if (args.key == undefined)
+				throw new Error('Must define key!!');
+
 			return _.extend({}, this, args);
 		},
 
-		sync: function(data, sheet) {
+		url: function() {
+			return this.baseURL + this.feed + this.key + this.worksheet + this.visibility + this.projection;
+		},
+
+		sync: function(data, worksheet) {
 			throw new Error('Not implemented yet!');
 		},
 
